0fb4e0591776e17edea6c3e7ad23095ff1cf0e27
Ehsan <e.rezaei.1981@gmail.com>
2023-08-31 21:55:19 +0200

    migration

---
+++ Launcher/Migrations/20230831192232_v3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shopping_Card_Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class v3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreationDateTimeUtc",
+                table: "Tb_User",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreationDateTimeUtc",
+                table: "Tb_User");
+        }
+    }
+}
